7ec6eea1d2b7ae852b9d6edfec0d02ad889c0ef9
OlaCharn <volha.charnukha@gmail.com>
2023-11-21 10:58:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  // 1 approach
+/*
+  useEffect( ()=>{
+    getQuote()
+  }, [])
+
+  const getQuote = async()=>{
+    const responce = await fetch("https://api.quotable.io/random");
+    const data = await responce.json();
+    //console.log(data)
+    setQuote(data.content)
+    setAuthor(data.author)
+  }
+*/
+  // 2 approach
+
+  /*
+  useEffect( ()=>{
+    const getQuote = async()=>{
+      const responce = await fetch("https://api.quotable.io/random");
+      const data = await responce.json();
+      setQuote(data.content);
+      setAuthor(data.author);
+    }
+    getQuote()
+  }, [])
+*/
+
+  // 3 approach
+  const getQuote = useCallback(async()=>{
+    const responce = await fetch("https://api.quotable.io/random");
+    const data = await responce.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  },[]);
+
+  useEffect(()=>{
+    getQuote()
+  }, [getQuote] )
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Quote'>
+        <h2> "{quote}" </h2>
+        <p> <i> {author} </i> </p>
+      </div>
+      <div>
+      <button onClick={getQuote} >GET YOUR QUOTE</button>
+      </div>   
     </div>
   );
 }
+
 
 export default App;
